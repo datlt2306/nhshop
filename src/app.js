5b192eb0e0e96afab6e99acc0a6d54965f1cfbb2
datlt2306 <datlt2306@gmail.com>
2024-03-26 22:52:43 +0700

    #6 - Cart

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import { connectDB } from "./config/db";
 import authRouter from "./routers/auth";
 import productRouter from "./routers/product";
 import categoryRouter from "./routers/category";
+import cartRouter from "./routers/cart";
 const app = express();
 dotenv.config();
 // middleware
@@ -21,4 +22,5 @@ connectDB(process.env.DB_URI);
 app.use("/api/v1", authRouter);
 app.use("/api/v1", productRouter);
 app.use("/api/v1", categoryRouter);
+app.use("/api/v1", cartRouter);
 export const viteNodeApp = app;
